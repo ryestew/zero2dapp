98239cd4322a4bb4e3b81c7fb1e169f243e83021
ryestew <rob@stupay.com>
2025-10-14 22:02:45 -0400

    Update token name

--- contracts/BuenaToken.sol
+++ contracts/BuenaToken.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts@5.4.0/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts@5.4.0/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts@5.4.0/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable, ERC20Permit {
+contract BuenaToken is ERC20, Ownable, ERC20Permit {
     constructor(address initialOwner)
         ERC20("BuenaToken", "BTK")
         Ownable(initialOwner)
